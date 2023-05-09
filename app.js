7bb0dff7b2970b4328697151630940f2bb4d1023
Nikhil Gunti <nikhilbabu829@gmail.com>
2023-05-09 14:59:38 +0530

    giving the tags group classes

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const screenArea = document.querySelector('.screenArea')
-const clear = document.querySelector('#clear')
 const off = document.querySelector('#off')
 const on = document.querySelector('#on')
 const AC = document.querySelector('#clear')
 
-const btn = document.querySelectorAll('button')
+let input = ''
+
+const display = (element)=>{
+
+}
+
+const btn = document.querySelectorAll('.numbers')
 btn.forEach(ele =>{
     ele.addEventListener('click',()=>{
         screenArea.innerText = ele.innerText
     })
 })
+
+const clear = document.querySelector('.clear')
+clear.addEventListener('click',()=>{
+    input = ''
+    screenArea.innerText = '0'
+})
+
+const operator = document.querySelectorAll('.')
 
