ac5987534e486c40bb5f504a3be8905ad98f3e76
Nikhil Gunti <nikhilbabu829@gmail.com>
2023-05-10 22:09:37 +0530

    adding todos and creating a variavle to store the clicks

--- app.js
+++ app.js
@@ -1,26 +1,63 @@
 const screenArea = document.querySelector('.screenArea')
-const off = document.querySelector('#off')
-const on = document.querySelector('#on')
-const AC = document.querySelector('#clear')
+const numbers = document.querySelectorAll('.numbers')
+const power = document.querySelectorAll('.power')
+const clear = document.querySelector('.clear')
+const body = document.querySelector('body')
+const operators = document.querySelectorAll('.operator')
 
 let input = ''
 
-const display = (element)=>{
+let powerIN = false
 
+//this is the power function ON/OFF 
+const powerCheck = (element)=>{
+    const power = `${element.target.innerText}`
+    if(power == 'OFF'){
+        screenArea.innerText = ''
+        input = ''
+        powerIN = false
+    }
+    else{
+        screenArea.innerText = '0'
+        powerIN = true
+    }
 }
 
-const btn = document.querySelectorAll('.numbers')
-btn.forEach(ele =>{
-    ele.addEventListener('click',()=>{
-        screenArea.innerText = ele.innerText
+//power btn click event
+power.forEach(btn =>{
+    btn.addEventListener('click',(ele)=>{
+        powerCheck(ele)
     })
 })
 
-const clear = document.querySelector('.clear')
-clear.addEventListener('click',()=>{
-    input = ''
-    screenArea.innerText = '0'
+// TODO: create a variable that hold all the digits pressed
+// TODO: create a event for operators
+// TODO: create a funtion that seperates the digits before and after the operator on equals click
+// TODO: create a function that passes the two side of digits to a function that does the arithmetic based on the opperator passed to it.
+
+//this is the digit clicks 
+numbers.forEach(ele =>{
+    ele.addEventListener('click', ()=>{
+        if(powerIN){
+            input += `${ele.innerText}`
+            screenArea.innerText = input
+        }
+    })
 })
 
-const operator = document.querySelectorAll('.')
+operators.forEach(op => {
+    op.addEventListener('click',()=>{
+        if(powerIN){
+            input += `${op.innerText}`
+            screenArea.innerText = input
+        }
+    })
+})
 
+//This is the All Clear route
+clear.addEventListener('click',()=>{
+    if(powerIN){
+        input = ''
+        screenArea.innerText = '0'
+    }    
+})
